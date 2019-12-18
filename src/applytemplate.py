26b0c2ab32befc74b8adc45592dc7ddfcb41062c
Alex Morega <alex@grep.ro>
2019-12-18 16:03:13 +0200

    Read template from the wiki

---
+++ src/applytemplate.py
@@ -0,0 +1,74 @@
+"""
+Apply the factsheet template to wiki pages
+
+https://taskman.eionet.europa.eu/projects/netpub/wiki/IT_service_factsheet_template
+"""
+
+import os
+import re
+
+from redminelib import Redmine
+
+
+def load_template(redmine):
+    factsheet_page = "IT_service_factsheet_template"
+    page = redmine.wiki_page.get(factsheet_page, project_id="netpub")
+    noise_pattern = (
+        r'<div id="wiki_extentions_header">\s*'
+        r'{{last_updated_at}} _by_ {{last_updated_by}}\s*'
+        r'</div>'
+    )
+    text = re.sub(noise_pattern, "", page.text).lstrip()
+
+    template_marker = (
+        r"\*Copy the template from below:\*\s+"
+        r"[-]+\s+"
+    )
+    (_ignore, template) = re.split(template_marker, text)
+
+    current = None
+    sections = []
+    for line in template.splitlines():
+        if line.startswith("h2."):
+            if current:
+                sections.append(current)
+
+            m = re.match(
+                r"h2\.\s+(?P<title>[^*]+)\s*(?P<mandatory>\*)?\s*$",
+                line,
+            )
+            current = {
+                "title": m.group("title"),
+                "mandatory": bool(m.group("mandatory")),
+                "lines": [],
+            }
+
+            continue
+
+        else:
+            if not current:
+                raise RuntimeError("Template must begin with `h2.`")
+            current["lines"].append(line)
+
+    if current:
+        sections.append(current)
+
+    return sections
+
+
+def main(config):
+    redmine = Redmine(
+        config["wiki_server"],
+        key=config["wiki_apikey"],
+        requests={"verify": True},
+    )
+    template = load_template(redmine)
+    print(template)
+
+
+if __name__ == "__main__":
+    config = {
+        "wiki_server": os.getenv("WIKI_SERVER", ""),
+        "wiki_apikey": os.getenv("WIKI_APIKEY", ""),
+    }
+    main(config)
