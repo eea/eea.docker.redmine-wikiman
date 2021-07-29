55241425b1a8e1312f124619cc2896d20a30d3d4
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2021-07-29 15:27:03 +0300

    add file logs

--- src/run_all.py
+++ src/run_all.py
@@ -13,9 +13,6 @@ import backupstacks
 from listhosts import RancherInstances
 from image_checker import ImageChecker
 
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
-log = logging.getLogger('')
-
 def run_apply_template(page, image_checker, dry_run):
     assert page, "Please provide a template"
 
@@ -44,8 +41,8 @@ def run_list_hosts(dry_run, environments):
         else:
             obj.write_page()
     else:
-        log.info("Content is the same, not saving")
-    log.info("Done list hosts")
+        logging.info("Content is the same, not saving")
+    logging.info("Done list hosts")
 
 def run_list_containers(image_checker, dry_run):
     listcontainers.main(image_checker, dry_run)
@@ -64,7 +61,9 @@ if __name__ == "__main__":
     page = None
     image_checker = ImageChecker()
 
-    log.setLevel(logging.INFO)
+    log_level = logging.INFO
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=log_level)
+    log = logging.getLogger()
     try:
         opts, args = getopt.getopt(sys.argv[1:], ":p:dvn")
     except getopt.GetoptError as err:
@@ -72,28 +71,66 @@ if __name__ == "__main__":
         sys.exit(2)
     for o, a in opts:
         if o == "-v":
-            log.setLevel(logging.DEBUG)
+            log_level = logging.DEBUG
+            log.setLevel(log_level)
         if o == "-n":
             dry_run = True
         if o == "-p":
             page = a
     if len(args) > 0:
         environments = args
+
+    formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s', 
+                                  '%m-%d-%Y %H:%M:%S')
+
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setLevel(log_level)
+    stdout_handler.setFormatter(formatter)
+
+    file_handler = logging.FileHandler('/var/log/list_stacks.log')
+    file_handler.setLevel(log_level)
+    file_handler.setFormatter(formatter)
+
+    log.addHandler(file_handler)
+    log.addHandler(stdout_handler)
+
         
     log.info('Running list stacks')
     run_list_stacks(dry_run)
 
+
+
+
     
     if os.getenv('GITLAB_CONFIG'):
         log.info('Running backup stacks')
+        log.removeHandler(file_handler)
+        file_handler = logging.FileHandler('/var/log/run_backup.log')
+        log.addHandler(file_handler)
         run_backup_stacks(dry_run)
 
+        
+    log.removeHandler(file_handler)
+    file_handler = logging.FileHandler('/var/log/list_hosts.log')
+    log.addHandler(file_handler)
+        
     log.info('Running list hosts')
     run_list_hosts(dry_run, environments)        
 
+        
+    log.removeHandler(file_handler)
+    file_handler = logging.FileHandler('/var/log/apply_template.log')
+    log.addHandler(file_handler)
+        
+        
     log.info('Running apply template')
     run_apply_template(page, image_checker, dry_run)
-
+        
+    log.removeHandler(file_handler)
+    file_handler = logging.FileHandler('/var/log/list_containers.log')
+    log.addHandler(file_handler)
+        
+        
     log.info('Running list containers')
     run_list_containers(image_checker, dry_run)
 
