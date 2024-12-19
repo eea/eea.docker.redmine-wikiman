be0ab1dadaefc7619390d34e1d76b72a0803132e
mereualexandra <mereu.alexandra95@gmail.com>
2024-12-19 15:12:56 +0100

    added redmine client auth

--- src/rancher2/auth.py
+++ src/rancher2/auth.py
@@ -3,6 +3,8 @@ import requests
 from dotenv import load_dotenv
 import os
 
+from redminelib import Redmine
+
 load_dotenv()
 
 
@@ -20,3 +22,40 @@ class RancherClient:
         )
         response.raise_for_status()
         return response.json()
+
+
+class RedmineClient:
+    def __init__(self):
+        self.base_url = os.getenv("WIKI_SERVER", "")
+        self.apikey = os.getenv("WIKI_APIKEY", "")
+        self.project_id = os.getenv("WIKI_PROJECT", "")
+        self.redmine_server = Redmine(
+            self.base_url, key=self.apikey, requests={"verify": True}
+        )
+
+    def has_changed(self, page_name, new_content):
+        """
+        Check if the content of the page has changed
+        :param page_name: The name of the page
+        :param new_content: text to compare
+
+        :return: True if the content has changed, False otherwise
+        """
+        server = self._redmine()
+        page = server.wiki_page.get(page_name, project_id=self.project_id)
+        old_content = page.text
+        marker = "_Do not update this page manually._"
+        return old_content.split(marker)[1] != new_content.split(marker)[1]
+
+    def write_page(self, page_name, content):
+        """
+        Write a page to the wiki server
+        :param page_name: The name of the page
+        :param content: The content of the page
+        """
+
+        self.redmine_server.wiki_page.update(
+            page_name,
+            project_id=self.project_id,
+            text=content,
+        )
--- src/rancher2/listnodes.py
+++ src/rancher2/listnodes.py
@@ -1,26 +1,26 @@
-import requests
-
-from src.rancher2.auth import RancherClient
+from src.rancher2.auth import RancherClient, RedmineClient
 
 
 class RancherNodes:
     def __init__(self):
-        self.client = RancherClient()
+        self.rancher_client = RancherClient()
+        self.redmine_client = RedmineClient()
+        self.page_name = "Rancher2 Nodes"
 
-    def get_clusters(self):
-        clusters_response = self.client.get("/v3/clusters")
+    def _get_clusters(self):
+        clusters_response = self.rancher_client.get("/v3/clusters")
         clusters_list = clusters_response["data"]
         return clusters_list
 
-    def get_nodes(self, cluster_id):
-        nodes_response = self.client.get(f"/v3/clusters/{cluster_id}/nodes")
+    def _get_nodes(self, cluster_id):
+        nodes_response = self.rancher_client.get(f"/v3/clusters/{cluster_id}/nodes")
         nodes_list = nodes_response["data"]
         return nodes_list
 
     def get_data(self):
-        clusters = self.get_clusters()
+        clusters = self._get_clusters()
         data = []
         for cluster in clusters:
-            nodes = self.get_nodes(cluster["id"])
+            nodes = self._get_nodes(cluster["id"])
             data.append({"cluster": cluster, "nodes": nodes})
         return data
